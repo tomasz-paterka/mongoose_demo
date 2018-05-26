efc96d472941dcb82b521997eeea1842ed713159
Tomasz Paterka <tomekpaterka@gmail.com>
2018-05-26 14:00:40 +0200

    changed path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
-	.get('/', (req, res) => res.render('pages/index'))
+	.get('/', (req, res) => res.send('Hello world'))
 	.get('/cool', (req, res) => res.send(cool()))
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
